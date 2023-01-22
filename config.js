4f0da2194b58f87c4ffab836e75dc97836ab19cb
Ha Nguyen <89948241+nthzz@users.noreply.github.com>
2023-01-23 06:40:34 +0700

    Add files via upload
    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Chị T giấu tên';
+const giftUrl = 'https://www.facebook.com/thuongvu13';
+const eventName = 'Happy new year';
+const titleCard = 'little coder';
+const contentCard = 'Nhân dịp năm mới 2023, chúc chị của iem luon vuituoi, luôn xênh đẹp, học tập tốt nhé ! Tym tym ';
+
+
+const giftImage = 'hot-girl.png';
+const qrImage = 'qr.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
